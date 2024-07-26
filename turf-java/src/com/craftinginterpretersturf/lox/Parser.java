e4276da551f17608e5cf975532810c0d1341c61e
lethalgem <iwc123@gmail.com>
2024-07-26 11:05:07 -0400

    Complete to start of 10.5

--- turf-java/src/com/craftinginterpretersturf/lox/Parser.java
+++ turf-java/src/com/craftinginterpretersturf/lox/Parser.java
@@ -136,6 +136,7 @@ public class Parser {
     private Stmt.Function function(String kind) {
         Token name = consume(IDENTIFIER, "Expect " + kind + " name.");
         consume(LEFT_PAREN, "Expect '(' after " + kind + " name.");
+        List<Token> parameters = new ArrayList<>();
         if (!check(RIGHT_PAREN)) {
             do {
                 if (parameters.size() >= 255) {
