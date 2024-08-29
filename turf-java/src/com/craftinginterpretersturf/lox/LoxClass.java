d715630d66a4f25ba0ea08c243e6fd0b29b9b852
lethalgem <iwc123@gmail.com>
2024-08-29 16:53:44 -0400

    Complete to start of 12.3

---
+++ turf-java/src/com/craftinginterpretersturf/lox/LoxClass.java
@@ -0,0 +1,18 @@
+package com.craftinginterpretersturf.lox;
+
+import java.util.List;
+import java.util.Map;
+
+class LoxClass {
+    final String name;
+
+    LoxClass(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+
+}
